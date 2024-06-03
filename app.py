bc602dc26b0d187dec36b9e3b5bc1d02e12fed22
Akash Thakur <117273921+akashthakur4553@users.noreply.github.com>
2024-06-03 23:43:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,26 @@
-import cv2
 import streamlit as st
-import numpy as np
-from PIL import Image
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration
 
+# Define a simple video transformer (for demonstration, does nothing)
+class IdentityTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        return frame
 
-def brighten_image(image, amount):
-    img_bright = cv2.convertScaleAbs(image, beta=amount)
-    return img_bright
+def main():
+    st.title("Real-Time Video Stream")
 
+    # Customize RTC configuration for better connectivity (optional)
+    rtc_configuration = RTCConfiguration(
+        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    )
 
-def blur_image(image, amount):
-    blur_img = cv2.GaussianBlur(image, (11, 11), amount)
-    return blur_img
+    # Create a WeBRTC component
+    webrtc_streamer(
+        key="example",
+        video_transformer_factory=IdentityTransformer,
+        rtc_configuration=rtc_configuration,  # Optional
+        media_stream_constraints={"video": True, "audio": False},
+    )
 
-
-def enhance_details(img):
-    hdr = cv2.detailEnhance(img, sigma_s=12, sigma_r=0.15)
-    return hdr
-
-
-def main_loop():
-    st.title("OpenCV Demo App")
-    st.subheader("This app allows you to play with Image filters!")
-    st.text("We use OpenCV and Streamlit for this demo")
-
-    blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-    brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
-    apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
-
-    image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
-    if not image_file:
-        return None
-
-    original_image = Image.open(image_file)
-    original_image = np.array(original_image)
-
-    processed_image = blur_image(original_image, blur_rate)
-    processed_image = brighten_image(processed_image, brightness_amount)
-
-    if apply_enhancement_filter:
-        processed_image = enhance_details(processed_image)
-
-    st.text("Original Image vs Processed Image")
-    st.image([original_image, processed_image])
-
-
-if __name__ == '__main__':
-    main_loop()
+if __name__ == "__main__":
+    main()
